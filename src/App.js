0c5a6fa00219883f429b1e487032182c8fb61cb7
mohamedarief0 <mrarief0110@gmail.com>
2024-11-12 11:54:52 +0530

    Route managing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import './App.css';
-
-function App() {
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/AboutCollege/AboutCollege";
+import Graduation from "./pages/Graduation/Graduation";
+// import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import "./css/main.css";
+export default function App() {
   return (
-    <><h1>hel</h1>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/காணொளி" element={<Graduation />} />
+      </Routes>
     </>
   );
 }
-
-export default App;
